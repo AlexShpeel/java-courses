eddd6ab1f31b3d94be2cf1864291eae00e9f5c72
AlexShpeel <shpeelj@gmail.com>
2018-08-21 11:22:28 +0200

    Choose Operation

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,23 @@
 public class Calculator{
 
-    private int result;
+    private double result = 0.0;
 
     public void add(int ... vars){
         for (Integer i: vars) {
             this.result += i;
         }
     }
+    public void sub(int a, int b){
+            this.result = a - b;
+    }
+    public void multi(int a, int b){
+        result = a * b;
+    }
+    public void div( double a, double b){
+        result = a / b;
+    }
 
-    public int getResult(){
+    public double getResult(){
         return this.result;
     }
 
--- InteractRunner.java
+++ InteractRunner.java
@@ -1,25 +1,51 @@
 import java.util.Scanner;
 
-public class InteractRunner{
+public class InteractRunner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        try{
+        try {
             Calculator calc = new Calculator();
             String exit = "no";
-            while (!exit.equals("yes")){
+            while (!exit.equals("yes")) {
                 System.out.println("Type first number");
                 String first = sc.next();
                 System.out.println("Type second number");
                 String second = sc.next();
+                System.out.println("Choose operation \n" +
+                        "1: addition\n" +
+                        "2: subtraction\n" +
+                        "3: multiplication\n" +
+                        "4: division");
+                int choice = Integer.parseInt(sc.next());
+                switch (choice) {
 
-                calc.add(Integer.valueOf(first), Integer.valueOf(second));
-                System.out.println("Summ = " + calc.getResult());
+                    case 1: {
+                        calc.add(Integer.valueOf(first), Integer.valueOf(second));
+                        System.out.println("Summ = " + calc.getResult());
+                        break;
+                    }
+                    case 2: {
+                        calc.sub(Integer.valueOf(first), Integer.valueOf(second));
+                        System.out.println("Subtraction = " + calc.getResult());
+                        break;
+                    }
+                    case 3: {
+                        calc.multi(Integer.valueOf(first), Integer.valueOf(second));
+                        System.out.println("Multiplication = " + calc.getResult());
+                        break;
+                    }
+                    case 4: {
+                        calc.div(Integer.valueOf(first), Integer.valueOf(second));
+                        System.out.println("Division = " + calc.getResult());
+                        break;
+                    }
+                }
                 calc.cleanResult();
                 System.out.println("Exit? yes/no...");
                 exit = sc.next();
             }
 
-        }finally {
+        } finally {
             sc.close();
         }
 
